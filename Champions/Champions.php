38838c17f5cc85cee3086de740d52db4601ee1c8
Jason Lewicki <lewicki.jason@gmail.com>
2015-04-10 21:54:08 +0000

    Simple Fiddlesticks stats added. Spells still needed.

---
+++ Champions/Champions.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace PerfectBuild\Champions;
+
+Class Champions{
+	
+	protected $gold;
+	protected $level;
+	protected $experience;
+	
+	protected $base_health;
+	protected $health_per_level;
+	protected $base_health_regen;
+	protected $health_regen_per_level;
+	
+	protected $base_mana;
+	protected $mana_per_level;
+	protected $base_mana_regen;
+	protected $mana_regen_per_level;
+	
+	protected $base_attack_damage;
+	protected $attack_damage_per_level;
+	protected $base_attack_speed;
+	
+	protected $base_armor;
+	protected $armor_per_level;
+	
+	protected $base_magic_resist;
+	protected $base_movement_speed;
+	
+	public function __construct() {
+		
+	}
+	
+	
+}
