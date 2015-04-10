51707a14abf7c27e5b3048207906d001786572d0
Jason Lewicki <lewicki.jason@gmail.com>
2015-04-10 19:22:01 +0000

    First commit. Basic File structure layed out.

---
+++ Configs/config_example.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Location and name of database INI file
+ */
+define('DATABASE_INI', '/var/.../application/root/configs/database_example.ini');
+
+/**
+ * Set the application root. This is the actual root of your application.
+ */
+define('APP_ROOT', '/var/.../application/root');
+
+/**
+ * Set the CryptoTraderHub root.
+ */
+define('PERFECT_BUILD_ROOT', '/var/.../perfect-build');
+
+/**
+ * Error reporting. Comment out to remove error reporting.
+ */
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
---
+++ run.php
@@ -0,0 +1,4 @@
+<?php
+
+include dirname(__FILE__).'/Configs/config.php';
+include PERFECT_BUILD_ROOT.DIRECTORY_SEPARATOR.'autoload.php';
