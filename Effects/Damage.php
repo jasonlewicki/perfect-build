4f82bc7b9515dc8bd95ed3fc4cab4a61b689d760
Jason Lewicki <lewicki.jason@gmail.com>
2015-04-24 21:17:49 +0000

    Still trying to figure out how to handle effects.

--- Effects/Damage.php
+++ Effects/Damage.php
@@ -5,7 +5,7 @@ namespace PerfectBuild\Effects;
 class Damage extends \PerfectBuild\Effects\Effects{
 		
 	// Constructor
-	public function __construct() {		
+	public function __construct($delay, $damage) {		
 		parent::__construct("Damage");			
 	}
 }
---
+++ Effects/Disable.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace PerfectBuild\Effects;
+
+class Disable extends \PerfectBuild\Effects\Effects{
+		
+	// Constructor
+	public function __construct() {		
+		parent::__construct("Disable");			
+	}
+}
