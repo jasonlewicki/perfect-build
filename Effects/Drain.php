5ea0ba4a6695298eb944630ecc495d1cf18cd5e5
Jason Lewicki <lewicki.jason@gmail.com>
2015-05-06 17:21:49 +0000

    Documentation.

--- Effects/Drain.php
+++ Effects/Drain.php
@@ -13,6 +13,8 @@ class Disable extends \PerfectBuild\Effects\Effect{
 		$this->duration = 5;
 		$this->interval = .5;		
 		
+		
+		// Where to put this?
 		//MAGIC DAMAGE PER SECOND: 60 / 90 / 120 / 150 / 180 (+ 45% AP)
 		//MAX MAGIC DAMAGE: 300 / 450 / 600 / 750 / 900 (+ 225% AP)
 		//HEAL FROM DAMAGE RATIO: 60 / 65 / 70 / 75 / 80%
