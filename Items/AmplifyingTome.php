435d1209ad130b80e2d547c24b9649ecff712dad
Jason Lewicki <lewicki.jason@gmail.com>
2015-05-21 18:02:48 +0000

    Basic items added.

---
+++ Items/AmplifyingTome.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace PerfectBuild\Effects;
+
+class AmplifyingTome extends \PerfectBuild\Items\Item{
+	
+	protected $interval;
+		
+	// Constructor
+	public function __construct($option_arr) {		
+		parent::__construct("Amplifying Tome");	
+			
+	}	
+	
+}
---
+++ Items/BlastingWand.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace PerfectBuild\Effects;
+
+class BlastingWand extends \PerfectBuild\Items\Item{
+	
+	protected $interval;
+		
+	// Constructor
+	public function __construct($option_arr) {		
+		parent::__construct("Blasting Wand");	
+			
+	}	
+	
+}
