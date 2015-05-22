3cb45e707de10b7a4907ff147d51b7463da09f5b
Jason Lewicki <lewicki.jason@gmail.com>
2015-05-22 16:28:30 +0000

    Item added.

---
+++ Items/ClothArmor.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace PerfectBuild\Effects;
+
+class ClothArmor extends \PerfectBuild\Items\Item{
+	
+	protected $interval;
+		
+	// Constructor
+	public function __construct($option_arr) {		
+		parent::__construct("Cloth Armor");	
+			
+	}	
+	
+}
