958e93b66b6d3ced78da876b118707ee2db85e9a
Jason Lewicki <lewicki.jason@gmail.com>
2015-06-16 14:24:02 +0000

    Added Brawler's Gloves.

---
+++ Items/BrawlersGloves.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace PerfectBuild\Items;
+
+clasBrawlersGloves extends \PerfectBuild\Items\Item{
+	
+	protected $interval;
+		
+	// Constructor
+	public function __construct() {		
+		parent::__construct("Brawler's Gloves");
+		$this->cost = 400;		
+		$this->sell_value = 280;	
+		$this->basic_effects_arr['critical_chance_percent'] = 8.0;	
+	}	
+	
+	public function activate($caster_obj, $mob_obj){
+		throw new \Exception('Un-Activatable Item Exeception');	
+	}
+}
