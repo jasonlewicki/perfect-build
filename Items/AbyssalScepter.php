9382af8c05bf27f1e39aec014bbd90272dcfca1e
Jason Lewicki <lewicki.jason@gmail.com>
2015-08-04 20:53:26 +0000

    Formatting.

--- Items/AbyssalScepter.php
+++ Items/AbyssalScepter.php
@@ -9,12 +9,12 @@ class AbyssalScepter extends \PerfectBuild\Items\Item{
 	// Constructor
 	public function __construct() {		
 		parent::__construct("Abyssal Scepter");	
-		$this->cost = 2440;	
-		$this->sell_value = 1708;	
+		$this->cost = 2440;
+		$this->sell_value = 1708;
 		$this->basic_effects_arr['ability_power'] = 70.0;
 		$this->basic_effects_arr['magic_resistance'] = 50.0;
 		$this->effects_arr[] = new \PerfectBuild\Effects\AbyssalScepter();
-	}	
+	}
 	
 	public function activate($caster_obj, $mob_obj){
 		throw new \Exception('Un-Activatable Item Exeception');	
